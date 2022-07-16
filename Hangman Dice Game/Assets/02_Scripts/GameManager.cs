82efb49b44db70cf1d05f1dfab5769ff1d33461a
tamago04 <ljyhinako@gmail.com>
2022-07-16 17:43:52 +0800

    Words list & choosing random word

--- Hangman Dice Game/Assets/02_Scripts/GameManager.cs
+++ Hangman Dice Game/Assets/02_Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    
+    public List<Word> words;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       ChooseWord(); 
+    }
+
+    public void ChooseWord()
+    {
+        Word word = new Word(WordGenerator.GetRandomWord());
+        Debug.Log(word.word);
     }
 
     // Update is called once per frame

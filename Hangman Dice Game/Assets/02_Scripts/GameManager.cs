3be076e9944df8255add546694f757a480f8637f
tamago04 <ljyhinako@gmail.com>
2022-07-16 19:13:23 +0800

    Editing GM script

--- Hangman Dice Game/Assets/02_Scripts/GameManager.cs
+++ Hangman Dice Game/Assets/02_Scripts/GameManager.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     
     public List<Word> words;
-    //private GameObject[] tilesAll;
-    //Object[] with only word tiles
     public GameObject[] tilesWord;
     string str = "Books";
     public char[] wordAsChars;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +26,8 @@ public class GameManager : MonoBehaviour
 
     public void SetTiles(string selectedWord)
     {
-        //populating letter tiles into the tiles array
-        //tilesAll = new GameObject[] {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z};
-
         Debug.Log("selected word is " + selectedWord);
+
         //convert word string into an array of separate characters
         wordAsChars = selectedWord.ToCharArray(0, str.Length);
         //create space for the array 
@@ -41,7 +38,12 @@ public class GameManager : MonoBehaviour
             //each letter is converted to a string so it can be used in GameObject.Find
             string letter = "" + wordAsChars[i];
             //find the gameobject that is of the same letter
-            tilesWord[i] = GameObject.Find(letter);
+            //tilesWord[i] = GameObject.Find(letter);
+
+            tilesWord[i] = new GameObject();
+            tilesWord[i].AddComponent<TextMeshProUGUI>().text = wordAsChars[i].ToString();
+
+            //can create a list of gameobjects for the tiles & it will be assigned in order
         }
     }
 

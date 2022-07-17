a0e4727f53a0171d43dad959bc9ecc582ce0b130
tamago04 <ljyhinako@gmail.com>
2022-07-17 11:34:10 +0800

    Added more words!

--- Hangman Dice Game/Assets/02_Scripts/WordGenerator.cs
+++ Hangman Dice Game/Assets/02_Scripts/WordGenerator.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class WordGenerator 
 {
 
-    private static string[] wordList = { "APPLE", "BREAD", "HONEY", "VIOLA", "PIANO", "THORN" };
+    private static string[] wordList = { "APPLE", "ADORE", "ABYSS", "AROMA", "ARMOR", "BEACH", "BEIGE", "BREAD", "BACON", "CREAM", "CREPE",
+    "CHAIR", "COAST", "COCOA", "DAIRY", "DANCE", "DENIM", "DEMON", "DEVIL", "DIARY", "DINER", "DISCO", "DREAM", "DRAKE", "ELDER", "ELBOW", 
+    "EMAIL", "FAIRY", "FAUNA", "FONDU", "FROST", "FLOOD", "FLOOR", "FLORA", "FENCE",
+    "GRAPE", "GNOME", "GOLEM", "HONEY", "IGLOO", "ICING", "INBOX", "JOKER", "JUICE", "KHAKI", "KOALA", "LEMON", "LILAC", "LIGHT", "LOTUS", "MANGO",
+    "MODEL", "NORTH", "NYMPH", "OASIS", "OLIVE", "OPERA", "OTTER", "PEONY", "PIANO", "QUEEN", "RADAR", "RAVEN", 
+    "STONE", "THORN", "UNION", "VIOLA", "WOUND", "YACHT", "ZEBRA" };
 
     public static string GetRandomWord()
     {
